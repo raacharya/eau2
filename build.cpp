3b300a04c2dedfa9ea08651796afeb3ce4b218b4
Rahul Acharya <rahul@Rahuls-MBP-2.fios-router.home>
2020-03-16 01:39:08 -0400

    changes

--- build.cpp
+++ build.cpp
@@ -14,11 +14,6 @@ enum class ParseState {
     FLAG_F,
     FLAG_FROM,
     FLAG_LEN,
-    FLAG_COL_TYPE,
-    FLAG_COL_IDX_COL,
-    FLAG_COL_IDX_OFF,
-    FLAG_MISSING_IDX_COL,
-    FLAG_MISSING_IDX_OFF
 };
 
 /**
@@ -55,18 +50,11 @@ void parse_size_t_arg(ssize_t* arg_loc, char* arg) {
  * @param col_idx_col, col_idx_off Pointer to result of parsing -print_col_idx
  * @param missing_idx_col, missing_idx_off Pointer to result of parsing -is_missing_idx
  */
-void parse_args(int argc, char* argv[], char** file, ssize_t* start, ssize_t* len,
-                ssize_t* col_type, ssize_t* col_idx_col, ssize_t* col_idx_off,
-                ssize_t* missing_idx_col, ssize_t* missing_idx_off) {
+void parse_args(int argc, char* argv[], char** file, ssize_t* start, ssize_t* len) {
     *file = nullptr;
     // -1 represents argument not provided
     *start = -1;
     *len = -1;
-    *col_type = -1;
-    *col_idx_col = -1;
-    *col_idx_off = -1;
-    *missing_idx_col = -1;
-    *missing_idx_off = -1;
 
     ParseState state = ParseState::DEFAULT;
 
@@ -80,12 +68,6 @@ void parse_args(int argc, char* argv[], char** file, ssize_t* start, ssize_t* le
                     state = ParseState::FLAG_FROM;
                 } else if (strcmp(arg, "-len") == 0) {
                     state = ParseState::FLAG_LEN;
-                } else if (strcmp(arg, "-print_col_type") == 0) {
-                    state = ParseState::FLAG_COL_TYPE;
-                } else if (strcmp(arg, "-print_col_idx") == 0) {
-                    state = ParseState::FLAG_COL_IDX_COL;
-                } else if (strcmp(arg, "-is_missing_idx") == 0) {
-                    state = ParseState::FLAG_MISSING_IDX_COL;
                 } else {
                     cli_assert(false);
                 }
@@ -103,26 +85,6 @@ void parse_args(int argc, char* argv[], char** file, ssize_t* start, ssize_t* le
                 parse_size_t_arg(len, arg);
                 state = ParseState::DEFAULT;
                 break;
-            case ParseState::FLAG_COL_TYPE:
-                parse_size_t_arg(col_type, arg);
-                state = ParseState::DEFAULT;
-                break;
-            case ParseState::FLAG_COL_IDX_COL:
-                parse_size_t_arg(col_idx_col, arg);
-                state = ParseState::FLAG_COL_IDX_OFF;
-                break;
-            case ParseState::FLAG_COL_IDX_OFF:
-                parse_size_t_arg(col_idx_off, arg);
-                state = ParseState::DEFAULT;
-                break;
-            case ParseState::FLAG_MISSING_IDX_COL:
-                parse_size_t_arg(missing_idx_col, arg);
-                state = ParseState::FLAG_MISSING_IDX_OFF;
-                break;
-            case ParseState::FLAG_MISSING_IDX_OFF:
-                parse_size_t_arg(missing_idx_off, arg);
-                state = ParseState::DEFAULT;
-                break;
             default:
                 cli_assert(false);
         }
@@ -130,61 +92,30 @@ void parse_args(int argc, char* argv[], char** file, ssize_t* start, ssize_t* le
     cli_assert(state == ParseState::DEFAULT);
 }
 
-/**
- * Gets the column at the given index from the given set. Terminates with an error print if which
- * is not valid.
- * @param set The ColumnSet to get from
- * @param which The column index to get
- */
-BaseColumn* getColumnChecked(ColumnSet* set, size_t which) {
-    if (which >= set->getLength()) {
-        printf("No such column\n");
-        exit(-1);
-    }
-    return set->getColumn(which);
-}
 
 /**
- * Terminates if the given column is not large enough to have the given entry index.
- * @param col The column
- * @param which The entry index
+ * function to build a dataframe from a sorer file
  */
-void checkColumnEntry(BaseColumn* col, size_t which) {
-    if (which >= col->getLength()) {
-        printf("No such entry\n");
-        exit(-1);
-    }
-}
-
-/**
- * The main function.
- */
-int main(int argc, char* argv[]) {
+DataFrame* buildFromFile(int argc, char* argv[]) {
     // Parse arguments
     char* filename = nullptr;
     // -1 represents argument not provided
     ssize_t start = -1;
     ssize_t len = -1;
-    ssize_t col_type = -1;
-    ssize_t col_idx_col = -1;
-    ssize_t col_idx_off = -1;
-    ssize_t missing_idx_col = -1;
-    ssize_t missing_idx_off = -1;
 
-    parse_args(argc, argv, &filename, &start, &len, &col_type, &col_idx_col, &col_idx_off,
-               &missing_idx_col, &missing_idx_off);
+    parse_args(argc, argv, &filename, &start, &len);
 
     // Check arguments
     if (filename == nullptr) {
         printf("No file provided\n");
-        return -1;
+        return nullptr;
     }
 
     // Open requested file
     FILE* file = fopen(filename, "r");
     if (file == NULL) {
         printf("Failed to open file\n");
-        return -1;
+        return nullptr;
     }
     fseek(file, 0, SEEK_END);
     size_t file_size = ftell(file);
@@ -205,21 +136,6 @@ int main(int argc, char* argv[]) {
         parser.guessSchema();
         parser.parseFile();
         ColumnSet* set = parser.getColumnSet();
-
-        // Print requested query
-        if (col_type != -1) {
-            ColumnType type = getColumnChecked(set, (size_t)col_type)->getType();
-            printf("%s\n", columnTypeToString(type));
-        } else if (col_idx_col != -1) {
-            BaseColumn* col = getColumnChecked(set, (size_t)col_idx_col);
-            checkColumnEntry(col, (size_t)col_idx_off);
-            col->printEntry((size_t)col_idx_off);
-        } else if (missing_idx_col != -1) {
-            BaseColumn* col = getColumnChecked(set, (size_t)missing_idx_col);
-            checkColumnEntry(col, (size_t)missing_idx_off);
-            bool present = col->isEntryPresent((size_t)missing_idx_off);
-            printf("%d\n", !present);
-        }
 
         Schema* s = new Schema();
         DataFrame df(*s);
@@ -261,5 +177,17 @@ int main(int argc, char* argv[]) {
 
         df.print();
         fclose(file);
+        return &df;
     }
+}
+
+/**
+ * Main entry point to the program
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, char* argv[]) {
+    DataFrame* df = buildFromFile(argc, argv);
+    return 1;
 }
