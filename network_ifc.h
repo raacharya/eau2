147e49620821a6c99656acd3e229d70e0418a5bb
Rahul Acharya <rahul@Rahuls-MBP-2.fios-router.home>
2020-04-22 20:24:13 -0400

    changes

---
+++ network_ifc.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "object.h"
+
+class NetworkIfc : public Object {
+    public:
+        virtual void register_node(size_t idx) {}
+
+        virtual size_t index() { assert(false); }
+
+        virtual void send_m(Message* msg) = 0;
+
+        virtual Message* recv_m() = 0;
+};
